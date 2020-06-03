5c3e8336cbe4f8f383d25723a419aa05ac412aa6
ho2ast <lucid7@hotmail.co.kr>
2020-06-03 19:02:34 +0900

    5_1th

--- src/main/java/com/exam/controller/TodolistController.java
+++ src/main/java/com/exam/controller/TodolistController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.exam.domain.TodoVO;
@@ -18,7 +19,12 @@ public class TodolistController {
 	@Autowired
 	TodoService todoService;
 	
-	@GetMapping("/wirte")
+	@GetMapping("/")
+	public String todoMain() {
+		return "todolist/todolist";
+	}
+	
+	@PostMapping("/wirte")
 	public String todolist(TodoVO todoVO, Model model) {
 		
 		List<TodoVO> list = todoService.writeTodo(todoVO);
